66caecbfc02a52cca3269200e2c51d07c547d023
adesh <aadan4762@gmail.com>
2023-06-23 11:31:49 +0300

    initial commit

---
+++ Users-service/src/main/java/com/adan/Usersservice/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.adan.Usersservice.service;
+
+
+import com.adan.Usersservice.entity.UserCredential;
+import com.adan.Usersservice.repository.UserCredentialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserCredentialRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    public String saveUser(UserCredential credential) {
+        credential.setPassword(passwordEncoder.encode(credential.getPassword()));
+        repository.save(credential);
+        return "user added to the system";
+    }
+
+    public String generateToken(String username) {
+        return jwtService.generateToken(username);
+    }
+
+    public void validateToken(String token) {
+        jwtService.validateToken(token);
+    }
+
+
+}
